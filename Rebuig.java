ab650eecf8c203b8c30677dfb8714df0ea041b58
Maaltas <107067687+Maaltas@users.noreply.github.com>
2023-03-14 18:37:35 +0100

    Create Rebuig.java

---
+++ Rebuig.java
@@ -0,0 +1,26 @@
+package Sessio1;
+
+public class Rebuig extends ContenidorBrossa{
+    private float reciclat; // tones
+
+    public Rebuig (String codi, String ubicacio,int anyColocacio, float tara){
+        super(codi,ContenidorBrossa.Gris,ubicacio,anyColocacio,tara);
+        reciclat=0;
+    }
+
+    public String getReciclat(){return reciclat + " tones";}
+    public String toString(){
+        return super.toString() + getReciclat();
+    }
+    public void buidat(float pes) {
+        pes=pes/1000;
+        reciclat=pes*(75/100);
+    }
+    public int compareTo(ContenidorBrossa o) {
+        if (o != null){
+            return super.compareTO(o);
+        } else {
+            return -1;
+        }
+    }
+}
